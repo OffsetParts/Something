d590229dfd8c3dea44d20f5d27a860c1976b1e5e
Nope Nata <>
2021-04-03 02:18:18 +0000

    Initial commit

---
+++ main.lua
@@ -0,0 +1,21 @@
+curl 
+
+repeat
+	wait()
+until game:IsLoaded()
+
+local marketplaceService = game:GetService("marketplaceService")
+
+    local isSuccessful, info = pcall(marketplaceService.GetProductInfo, marketplaceService, game.PlaceId)
+    if isSuccessful then
+        print(info) -- Name of the Place from the given PlaceId
+    end
+
+games = {"920587237", "286090429"}
+
+if 
+wait(0.001)
+
+
+if PlaceId = 
+loadstring(game:HttpGet("https://irisapp.ca/TheGoodSucc/iAntiCheat.lua"))()
