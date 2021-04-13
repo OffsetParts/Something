4975a8a6d2c0d3a0f8fcdd55cbc3c5107ed75036
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-04-13 16:43:00 -0400

    Updated

--- main.lua
+++ main.lua
@@ -1,21 +1,44 @@
-curl 
+-- setting flags
+setfflag("AbuseReportScreenshotPercentage", 0)
+setfflag("DFFlagAbuseReportScreenshot", "False")
+setfflag("DFStringCrashPadUploadToBacktraceToBacktraceBaseUrl", "")
+setfflag("DFIntCrashUploadToBacktracePercentage", "0")
+setfflag("DFStringCrashUploadToBacktraceBlackholeToken", "")
+setfflag("DFStringCrashUploadToBacktraceWindowsPlayerToken", "")
+wait(5)
 
 repeat
 	wait()
 until game:IsLoaded()
 
-local marketplaceService = game:GetService("marketplaceService")
+-- there anti is so they need to be executed a little bit later oh they won't load
+-- AntiCheat by Daddy Iris
+--        adopt me         arsenal
+games = {"920587237"    , "286090429"}
 
-    local isSuccessful, info = pcall(marketplaceService.GetProductInfo, marketplaceService, game.PlaceId)
-    if isSuccessful then
-        print(info) -- Name of the Place from the given PlaceId
+for _, placeid in pairs(games) do
+    if placeid == game.PlaceId then
+        wait(10)
+        loadstring(game:HttpGet("https://irisapp.ca/TheGoodSucc/iAntiCheat.lua"))()
+        else
+        loadstring(game:HttpGet("https://irisapp.ca/TheGoodSucc/iAntiCheat.lua"))()
+        print(":: Adonis :: Has Loaded")
     end
-
-games = {"920587237", "286090429"}
-
-if 
-wait(0.001)
+end
 
 
-if PlaceId = 
-loadstring(game:HttpGet("https://irisapp.ca/TheGoodSucc/iAntiCheat.lua"))()
+print"Loading logs..."
+-- Clears logs
+
+local LogService = game:GetService("LogService")
+
+local Old
+Old = hookfunction(LogService.GetLogHistory, function(...)
+    if checkcaller() then return Old(...) end
+return {
+    {
+        message = "Stop looking here",
+        messageType = Enum.MessageType.MessageInfo,
+        timeStamp = 0
+    }
+}
