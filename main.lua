fead43ad09fffad792fcf41143044ca65e1c3a6f
Nope Nata <lifeisalie309@gmail.com>
2021-05-02 21:29:11 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -19,10 +19,30 @@ games = {"920587237","286090429"}
 for _, placeid in pairs(games) do
     if placeid == game.PlaceId then
         wait(20)
-        loadstring(game:HttpGet("https://irisapp.ca/TheGoodSucc/iAntiCheat.lua"))()
+			getgenv().BypassSettings = {
+			   ["Crystal AntiCheat"] = true,
+			   ["Adonis"] = true,
+
+			   ["Anti-Obfuscated Scripts"] = false,-- CANNOT BE ENABLED WITH CRYSTAL This will block any obfuscated script on the client from running (Not executed by your exploit thought)
+			   ["Random"] = true, -- Will disallow scripts calling, GetPropertyChanged signal on WalkSpeed, JumpPower, Gravity, Health, LogService
+
+			   ["Enable Kill Logs"] = true, -- Say if you want to get told what's bypassed
+			}
+
+			loadstring(game:HttpGet("https://irisapp.ca/TheGoodSucc/iAntiCheat.lua"))()
 		print(":: Adonis :: Has Loaded")
         else
-        loadstring(game:HttpGet("https://irisapp.ca/TheGoodSucc/iAntiCheat.lua"))()
+		getgenv().BypassSettings = {
+		   ["Crystal AntiCheat"] = true,
+		   ["Adonis"] = true,
+
+		   ["Anti-Obfuscated Scripts"] = false,-- CANNOT BE ENABLED WITH CRYSTAL This will block any obfuscated script on the client from running (Not executed by your exploit thought)
+		   ["Random"] = true, -- Will disallow scripts calling, GetPropertyChanged signal on WalkSpeed, JumpPower, Gravity, Health, LogService
+
+		   ["Enable Kill Logs"] = true, -- Say if you want to get told what's bypassed
+		}
+
+		loadstring(game:HttpGet("https://irisapp.ca/TheGoodSucc/iAntiCheat.lua"))()
         print(":: Adonis :: Has Loaded")
     end
 end
@@ -45,4 +65,5 @@ return {
 }
 end)
 
+-- Semi Net bypass
 hookfunction(setsimulationradius, function(a,b) end)
