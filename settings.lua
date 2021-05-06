b005c294b6b4b799b64a6ea1d1f687d74cad1830
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-05-06 18:38:03 -0400

    Update settings.lua

--- settings.lua
+++ settings.lua
@@ -3,7 +3,7 @@
 		   ["Adonis"] = true,
 
 		   ["Anti-Obfuscated Scripts"] = true,-- CANNOT BE ENABLED WITH CRYSTAL This will block any obfuscated script on the client from running (Not executed by your exploit thought)
-		   ["Random"] = true, -- Will disallow scripts calling, GetPropertyChanged signal on WalkSpeed, JumpPower, Gravity, Health, LogService
+		   ["Random"] = false, -- Will disallow scripts calling, GetPropertyChanged signal on WalkSpeed, JumpPower, Gravity, Health, LogService
 
 		   ["Enable Kill Logs"] = true, -- Say if you want to get told what's bypassed
 		}
