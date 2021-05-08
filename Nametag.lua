d8ea033f4bfa8a9b4e3494d9a7f2f7170055f9ef
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-05-08 18:29:12 -0400

    Update Nametag.lua

--- Nametag.lua
+++ Nametag.lua
@@ -1,1 +1,11 @@
+local Player = game:GetService("Players").LocalPlayer
+local char = Player.Character
 
+while true do
+    for i, v in pairs(char:GetDescendants()) do
+        if v:IsA("BillboardGui") then
+            v:Destroy()
+        end
+    end
+    wait(1)
+end
