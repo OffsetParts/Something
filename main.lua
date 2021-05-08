926a515c83b8f49eaa3e5e2af05494dbd36b13f1
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-05-08 08:32:29 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -7,42 +7,39 @@ setfflag("DFIntCrashUploadToBacktracePercentage", "0")
 setfflag("DFStringCrashUploadToBacktraceBlackholeToken", "")
 setfflag("DFStringCrashUploadToBacktraceWindowsPlayerToken", "")
 wait(0.1)
-
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/TrillyX/Resources/main/NFLib"))()
--- Free Notification system by trill and Mlemix they are gay don't trust them ^^
--- jk
-
 -- there anti is so they need to be executed a little bit later or they won't load
--- AntiCheat by Daddy Iris
-local placeid1 = 2772166173 -- Fort brag
-local placeid2 = 920587237 -- Adopt Me
-local placeid3 = 286090429 -- Arsenal
-local placeid4 = 6539893534 -- Testing
-local placeid5 = 6006653296 -- -- Frot Bradley
+-- AntiCheat by IrisV3rm V2
 -- if its anything else if will just execute default
-
-
-for _, placeid in pairs(game.PlaceId) do
-    print("what")
-    if placeid == placeid5 then
-	-- crystal
-	Library:Notification("CAC bypass Injected", "Crystal AntiCheat Bypass by Scrumptious", 5, Color3.fromRGB(255, 255 ,255))
-	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Input50/AntiCheatBypass/master/Crystal.lua?token=AKSKDDX2SQX76BH5HJKSB4DAT27Z6'),true))()
-	elseif placeid == placeid1 or placeid2 or placeid3 or placeid4 then
-	-- adonis)
-	Library:Notification("AN bypass Injected", "Adonis AntiCheat Bypass by Scrumptious", 5, Color3.fromRGB(255, 255 ,255))
-	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Input50/AntiCheatBypass/master/Adonis.lua?token=AKSKDDUHRYODOESLTM4Z7LTAT257A'),true))()
-	else
-	Library:Notification("UN bypass Injected", "Universal AntiCheat Bypass by Scrumptious", 5, Color3.fromRGB(255, 255 ,255))
-	loadstring(game:HttpGet(('https://raw.githubusercontent.com/Input50/AntiCheatBypass/master/default.lua?token=AKSKDDWFTJRCFIQC7BM3PBLAT3BLA'),true))()
-	end
-    return placeid
+local bgames = {
+    crystal = {
+        Name = "Crystal",
+        PlaceIDs = {6006653296},
+        ScriptToRun = "https://raw.githubusercontent.com/Input50/AntiCheatBypass/master/Crystal.lua?token=AKSKDDX2SQX76BH5HJKSB4DAT27Z6",
+    },
+    
+    adonis = {
+        Name = "Adonis",
+        PlaceIDs = {2772166173, 920587237, 286090429, 6539893534},
+        ScriptToRun = "https://raw.githubusercontent.com/Input50/AntiCheatBypass/master/Adonis.lua?token=AKSKDDUHRYODOESLTM4Z7LTAT257A",
+    },
+}
+un = true
+for _, bgame in pairs(bgames) do
+    for _, placeid in pairs(bgame.PlaceIDs) do
+        if placeid == game.PlaceId then
+            loadstring(game:HttpGet((bgame.ScriptToRun),true))()
+            print("Loaded "..bgame.Name)
+            un = false
+        end
+    end
 end
-
+if un then
+    loadstring(game:HttpGet(('https://raw.githubusercontent.com/Input50/AntiCheatBypass/master/default.lua?token=AKSKDDWFTJRCFIQC7BM3PBLAT3BLA'),true))()
+    print("Loaded ".. bgame.Name)
+end
 print("Loading Logs ...")
 -- changes locallogs for anyone who trys to get it it won't change for your LogHistory
 -- Full protection with sensible logs that are common to confuse any dev or admin
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/Input50/AntiCheatBypass/master/Logs.lua?token=AKSKDDU2HRCE7CGWHY7YMB3AT25QI'),true))()
-
 -- Semi Net bypass
 hookfunction(setsimulationradius, function(a,b) end)
