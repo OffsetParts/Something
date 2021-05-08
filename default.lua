074020ec0911cf5aa2d40574d5d9a0842f09a0ba
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-05-08 16:03:16 -0400

    Update default.lua

--- default.lua
+++ default.lua
@@ -1,3 +1,4 @@
+wait(5)
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/TrillyX/Resources/main/NFLib"))()
 
 getgenv().BypassSettings = {
@@ -7,9 +8,9 @@ getgenv().BypassSettings = {
    ["Anti-Obfuscated Scripts"] = false,-- CANNOT BE ENABLED WITH CRYSTAL This will block any obfuscated script on the client from running (Not executed by your exploit thought)
    ["Random"] = true, -- Will disallow scripts calling, GetPropertyChanged signal on WalkSpeed, JumpPower, Gravity, Health, LogService
 
-   ["Enable Kill Logs"] = true, -- Say if you want to get told what's bypassed
+   ["Enable Kill Logs"] = true, -- Say if you want to get told what's bypassedhttps://github.com/Input50/AntiCheatBypass/blob/master/default.lua
 }
 
 loadstring(game:HttpGet("https://irisapp.ca/TheGoodSucc/iAntiCheat.lua"))()
 
-Library:Notification("UN bypass Injected", "Universal AntiCheat Bypass by Scrumptious", 5, Color3.fromRGB(255, 255 ,255))
+Library:Notification("UN bypass Injected", "Universal AntiCheat Bypass by Scrumptious", 10, Color3.fromRGB(255, 255 ,255))
