8af0457b4545240a8567b7f2edce67e25e0ed945
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-05-08 18:42:12 -0400

    Update Nametag.lua

--- Nametag.lua
+++ Nametag.lua
@@ -1,11 +1,21 @@
+-- get game
+local marketplaceService = game:GetService("MarketplaceService")
+local place  = game.PlaceId
+local isSuccessful, info = pcall(marketplaceService.GetProductInfo, marketplaceService, place)
+-- define player
 local Player = game:GetService("Players").LocalPlayer
 local char = Player.Character
 
+if isSuccessful then
+    print(info.Name .. ": Nametags has been cleared")
+end
+
+-- get nametag within the local player character and destory it
 while true do
     for i, v in pairs(char:GetDescendants()) do
         if v:IsA("BillboardGui") then
             v:Destroy()
         end
     end
-    wait(1)
+    wait(5)
 end
