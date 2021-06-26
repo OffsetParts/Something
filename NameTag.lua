ede5a9229d8c997e7cff6cebe1279ebd672a200e
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-06-26 08:59:13 -0400

    Update and rename Dogwater.lua to NameTag.lua

---
+++ NameTag.lua
@@ -0,0 +1,21 @@
+if not game.Loaded then
+    game.Loaded:Wait()
+end
+wait(7.5)
+
+local player = game.Players.LocalPlayer
+local charc  = player.Character
+charc.Humanoid.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.None
+-- define player
+
+local plr = game:GetService("Players").LocalPlayer
+wait(0.5)
+plr.CharacterAdded:Connect(function(charca)
+	wait(3)
+	for index, v in pairs(charca:GetDescendants()) do
+		if v:IsA("BillboardGui") then
+			v:Destroy()
+			print("NameTag Removed")
+		end
+	end
+end)
