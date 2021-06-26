69a5ac2d6b6c22836b893fa13108e40f9fb2fd14
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-06-25 21:47:42 -0400

    Update DomainV2.lua

--- DomainV2.lua
+++ DomainV2.lua
@@ -1,5 +1,20 @@
 wait(2.5)
+-- light or dark mode
 local light = false
+-- individual settings for light or dark mode
+
+if light then
+startupsound = 1364317494
+theme = true
+themedata = {
+ThemeName = "Light Mode",
+BGColor1 = Color3.fromRGB(0,0,0),
+BGColor2 = Color3.fromRGB(64, 78, 237),
+LogoIcon = 000000,
+}
+wait(0.01)
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Input50/Something/master/Light.lua'),true))()
+elseif not light then
 startupsound = 1364317494
 theme = true
 themedata = {
@@ -8,9 +23,6 @@ BGColor1 = Color3.fromRGB(0,0,0),
 BGColor2 = Color3.fromRGB(255, 153, 0),
 LogoIcon = 000000,
 }
-
-if light then 
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/Input50/Something/master/Light.lua'),true))()
-elseif not light then
+wait(0.01)
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/Input50/Something/master/DomainV2.lua'),true))()
 end
