ec15b9e418f3a33378878d7f7e8f8d4dd23a958c
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-06-26 19:18:45 -0400

    Update NameTag.lua

--- NameTag.lua
+++ NameTag.lua
@@ -11,7 +11,7 @@ charc.Humanoid.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.None
 local plr = game:GetService("Players").LocalPlayer
 wait(0.5)
 plr.CharacterAdded:Connect(function(charca)
-local HRP = charc:WaitForChild("HumanoidRootPart")
+	wait(2)
 	for index, v in pairs(charca:GetDescendants()) do
 		if v:IsA("BillboardGui") then
 			v:Destroy()
