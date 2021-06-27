f6885a2c9386ff58654c4b2efa9808197923a42a
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-06-26 22:08:27 -0400

    Update Domain.lua

--- Domain.lua
+++ Domain.lua
@@ -9,7 +9,7 @@ BGColor2 = Color3.fromRGB(64, 78, 237),
 LogoIcon = 000000,
 }
 wait(0.01)
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/Input50/Something/master/Light.lua'),true))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/Something/master/Light.lua"),true))()
 elseif not light then
 startupsound = 1364317494
 theme = true
@@ -21,6 +21,6 @@ LogoIcon = 000000,
 }
 wait(0.01)
  print("Testing2")
-loadstring(game:HttpGet(('https://raw.githubusercontent.com/Input50/Something/master/DomainV2.lua'),true))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/Something/master/DomainV2.lua"),true))()
  print("Testing3")
 end
