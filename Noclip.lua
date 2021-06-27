c62fc989cfc3d44f3b0e969e297ed2565c6a893c
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-06-26 22:45:01 -0400

    Update Noclip.lua

--- Noclip.lua
+++ Noclip.lua
@@ -1,16 +1,108 @@
-if not game.Loaded then
-    game.Loaded:Wait()
+--Made by Scrumptious#0001
+local runDummyScript = function(f,scri)
+local oldenv = getfenv(f)
+local newenv = setmetatable({}, {
+__index = function(_, k)
+if k:lower() == 'script' then
+return scri
+else
+return oldenv[k]
+end
+end
+})
+setfenv(f, newenv)
+ypcall(function() f() end)
+end
+cors = {}
+mas = Instance.new("Model",game:GetService("Lighting")) 
+mas.Name = "CompiledModel"
+o1 = Instance.new("HopperBin")
+o2 = Instance.new("LocalScript")
+o1.Name = "Noclip"
+o1.Parent = mas
+o2.Name = "NoclipScript"
+o2.Parent = o1
+table.insert(cors,coroutine.create(function()
+wait()
+runDummyScript(function()
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+-- @CloneTrooper1019, 2015
+------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+local c = workspace.CurrentCamera
+local player = game.Players.LocalPlayer
+local userInput = game:GetService("UserInputService")
+local rs = game:GetService("RunService")
+local starterPlayer = game:GetService("StarterPlayer")
+
+local selected = false
+local speed = 100
+local lastUpdate = 0
+
+function getNextMovement(deltaTime)
+	local nextMove = Vector3.new()
+	-- Left/Right
+	if userInput:IsKeyDown("A") or userInput:IsKeyDown("Left") then
+		nextMove = Vector3.new(-1,0,0)
+	elseif userInput:IsKeyDown("D") or userInput:IsKeyDown("Right") then
+		nextMove = Vector3.new(1,0,0)
+	end
+	-- Forward/Back
+	if userInput:IsKeyDown("W") or userInput:IsKeyDown("Up") then
+		nextMove = nextMove + Vector3.new(0,0,-1)
+	elseif userInput:IsKeyDown("S") or userInput:IsKeyDown("Down") then
+		nextMove = nextMove + Vector3.new(0,0,1)
+	end
+	-- Up/Down
+	if userInput:IsKeyDown("Space") then
+		nextMove = nextMove + Vector3.new(0,1,0)
+	elseif userInput:IsKeyDown("LeftControl") then
+		nextMove = nextMove + Vector3.new(0,-1,0)
+	end
+	return CFrame.new( nextMove * (speed * deltaTime) )
 end
 
-wait(5)
-local plr = game:GetService("Players").LocalPlayer
+function onSelected()
+	local char = player.Character
+	if char then
+		local humanoid = char:WaitForChild("Humanoid")
+		local root = char:WaitForChild("HumanoidRootPart")
+		currentPos = root.Position
+		selected = true
+		root.Anchored = true
+		lastUpdate = tick()
+		humanoid.PlatformStand = true
+		while selected do
+			wait()
+			local delta = tick()-lastUpdate
+			local look = (c.Focus.p-c.CoordinateFrame.p).unit
+			local move = getNextMovement(delta)
+			local pos = root.Position
+			root.CFrame = CFrame.new(pos,pos+look) * move
+			lastUpdate = tick()
+		end
+		root.Anchored = false
+		root.Velocity = Vector3.new()
+		humanoid.PlatformStand = false
+	end
+end
 
-wait(0.5)
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/AntiCheatBypass/master/Noclip.lua"),true))()
+function onDeselected()
+	selected = false
+end
 
-
-plr.CharacterAdded:Connect(function()
-wait(0.75)
-	local REP = plr.Character:WaitForChild("HumanoidRootPart")
-	loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/AntiCheatBypass/master/Noclip.lua"),true))()
-end)
+script.Parent.Selected:connect(onSelected)
+script.Parent.Deselected:connect(onDeselected)
+end,o2)
+end))
+mas.Parent = workspace
+mas:MakeJoints()
+local mas1 = mas:GetChildren()
+for i=1,#mas1 do
+	mas1[i].Parent = game:GetService("Players").LocalPlayer.Backpack 
+	ypcall(function() mas1[i]:MakeJoints() end)
+end
+mas:Destroy()
+for i=1,#cors do
+coroutine.resume(cors[i])
+end
