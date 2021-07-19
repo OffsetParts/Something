22fffe45ff2751dc566f3f13020d31cf559994e5
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-07-19 19:37:31 -0400

    Add files via upload

---
+++ Anti-Kick.lua
@@ -0,0 +1,9 @@
+local Old
+Old = hookmetamethod(game, "__namecall", function(Self, ...) 
+    if getnamecallmethod() == "Kick" then 
+        return wait(9e8)
+    end
+
+    return Old(Self, ...)
+end)
+print("Anti-Kick Established")
