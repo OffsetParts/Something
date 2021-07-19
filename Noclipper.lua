893687952d3df2adb62ba89b77ac120b569db5d3
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-07-19 18:42:23 -0400

    Update Noclipper.lua

--- Noclipper.lua
+++ Noclipper.lua
@@ -1,7 +1,7 @@
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/AntiCheatBypass/master/Noclip.lua"),true))()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/Something/master/Noclip.lua"),true))()
 
 plr.CharacterAdded:Connect(function()
 wait(0.5)
     local REP = plr.Character:WaitForChild("HumanoidRootPart")
-    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/AntiCheatBypass/master/Noclip.lua"),true))()
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/Something/master/Noclip.lua"),true))()
 end)
