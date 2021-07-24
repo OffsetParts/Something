53cf151a409cb7a7af3377e8a442e28a3b748c92
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-07-23 23:48:56 -0400

    Update Noclipper.lua

--- Noclipper.lua
+++ Noclipper.lua
@@ -1,7 +1,6 @@
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/Something/master/Noclip.lua"),true))()
 
 plr.CharacterAdded:Connect(function()
-wait(0.5)
-    local REP = plr.Character:WaitForChild("HumanoidRootPart")
+wait(3)
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/Something/master/Noclip.lua"),true))()
 end)
