fa48a5a1e91e4c2f9a782edc701a66bb814638cf
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-07-23 23:57:13 -0400

    Update Noclipper.lua

--- Noclipper.lua
+++ Noclipper.lua
@@ -2,6 +2,6 @@ loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/Something/ma
 
 local plr = game:GetService("Players").LocalPlayer
 plr.CharacterAdded:Connect(function()
-wait(3)
+wait(1)
     loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/Something/master/Noclip.lua"),true))()
 end)
