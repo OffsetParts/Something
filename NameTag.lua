6353c837d0d2288e21bec03a35d2ce76f9a92c7d
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-07-27 22:13:33 -0400

    Update NameTag.lua

--- NameTag.lua
+++ NameTag.lua
@@ -2,22 +2,22 @@ local player = game:GetService("Players").LocalPlayer
 local charc  = player.Character
 charc.Humanoid.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.None
 
-for index, v in pairs(charc:GetDescendants()) do
-	if v:IsA("BillboardGui") then
-	v:Destroy()
-	print("NameTag Removed")
-	end
-end
--- define player
+local bgames = {
+    blacklisted = {
+        Name = "Blacklisted",
+        PlaceIDs = {5580097107},
+        ScriptToRun = "https://raw.githubusercontent.com/Input50/Something/master/NoNameTag.lua",
+    }
+}
 
-local plr = game:GetService("Players").LocalPlayer
-wait(0.5)
-plr.CharacterAdded:Connect(function(charca)
-	wait(4.5)
-	for index, v in pairs(charca:GetDescendants()) do
-		if v:IsA("BillboardGui") then
-			v:Destroy()
-			print("NameTag Removed")
-		end
-	end
-end)
+for _, bgame in pairs(bgames) do
+    for _, placeid in pairs(bgame.PlaceIDs) do
+        if placeid ~= game.PlaceId then
+            loadstring(game:HttpGet((bgame.ScriptToRun),true))()
+            bl = false
+	elseif placeid == game.PlaceId then
+		print("(4b)Nametag cannot not be removed due to blacklist")
+		break
+        end
+    end
+end
