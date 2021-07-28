1947b5fb8e22bce839b7e45c9dd0141bb73bf935
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-07-27 22:37:15 -0400

    Update NoNameTag.lua

--- NoNameTag.lua
+++ NoNameTag.lua
@@ -5,7 +5,9 @@ charc.Humanoid.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.None
 for index, v in pairs(charc:GetDescendants()) do
 	if v:IsA("BillboardGui") then
 	v:Destroy()
-	print("NameTag Removed")
+		if Print then
+			print("NameTag Removed")
+		end
 	end
 end
 
@@ -24,6 +26,9 @@ plr.CharacterAdded:Connect(function(charca)
 				break
 			else
 				v:Destroy()
+				print("NameTag Removed")
+			end
+			if Print then
 				print("NameTag Removed")
 			end
 		end
