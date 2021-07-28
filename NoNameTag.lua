67bcabe7f7c5f0d20c6517a13c564bed14dd2ae1
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-07-27 22:04:39 -0400

    Update NoNameTag.lua

--- NoNameTag.lua
+++ NoNameTag.lua
@@ -4,3 +4,15 @@ for index, v in pairs(charc:GetDescendants()) do
 	print("NameTag Removed")
 	end
 end
+
+local plr = game:GetService("Players").LocalPlayer
+wait(0.5)
+plr.CharacterAdded:Connect(function(charca)
+	wait(4.5)
+	for index, v in pairs(charca:GetDescendants()) do
+		if v:IsA("BillboardGui") then
+			v:Destroy()
+			print("NameTag Removed")
+		end
+	end
+end)
