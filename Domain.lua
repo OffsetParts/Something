f7888c4567498f56dced3179a250580acd14e4a7
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-08-05 04:09:17 -0400

    Update Domain.lua

--- Domain.lua
+++ Domain.lua
@@ -1,5 +1,9 @@
+-- light or dark mode
+local light = false
+-- individual settings for light or dark mode
+
 if light then
-startupsound = 1364317494
+--startupsound = 6205717931
 theme = true
 themedata = {
 ThemeName = "Light Mode",
@@ -8,9 +12,9 @@ BGColor2 = Color3.fromRGB(64, 78, 237),
 LogoIcon = 000000,
 }
 wait(0.01)
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/Something/master/Light.lua"),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Input50/Something/master/Light.lua'),true))()
 elseif not light then
-startupsound = 1364317494
+--startupsound = 6205717931
 theme = true
 themedata = {
 ThemeName = "Pornhub",
@@ -19,5 +23,5 @@ BGColor2 = Color3.fromRGB(255, 153, 0),
 LogoIcon = 000000,
 }
 wait(0.01)
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/Something/master/DomainV2.lua"),true))()
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/Input50/Something/master/DomainV2.lua'),true))()
 end
