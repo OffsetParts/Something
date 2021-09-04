b9c70c4b161d1b6558733f36bfb75ed349042fe1
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-09-04 19:13:36 -0400

    Update AAC.lua

--- AAC.lua
+++ AAC.lua
@@ -5,7 +5,7 @@ getgenv().BypassSettings = {
    ["Adonis"] = true,
 
    ["Anti-Obfuscated Scripts"] = false,-- CANNOT BE ENABLED WITH CRYSTAL This will block any obfuscated script on the client from running (Not executed by your exploit thought)
-   ["Random"] = false, -- Will disallow scripts calling, GetPropertyChanged signal on WalkSpeed, JumpPower, Gravity, Health, LogService
+   ["Random"] = true, -- Will disallow scripts calling, GetPropertyChanged signal on WalkSpeed, JumpPower, Gravity, Health, LogService
 
    ["Enable Kill Logs"] = true, -- Say if you want to get told what's bypassed
 }
