f15f8a515621ec71b5910ee575d5a64d4cbdca61
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-09-04 19:13:11 -0400

    Wording

--- CAC.lua
+++ CAC.lua
@@ -2,14 +2,14 @@ local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/Input
 
 getgenv().BypassSettings = {
    ["Crystal AntiCheat"] = true,
-   ["Adonis"] = true,
+   ["Adonis"] = false,
 
    ["Anti-Obfuscated Scripts"] = false,-- CANNOT BE ENABLED WITH CRYSTAL This will block any obfuscated script on the client from running (Not executed by your exploit thought)
-   ["Random"] = true, -- Will disallow scripts calling, GetPropertyChanged signal on WalkSpeed, JumpPower, Gravity, Health, LogService
+   ["Random"] = false, -- Will disallow scripts calling, GetPropertyChanged signal on WalkSpeed, JumpPower, Gravity, Health, LogService
 
    ["Enable Kill Logs"] = true, -- Say if you want to get told what's bypassed
 }
 
 loadstring(game:HttpGet("https://irisapp.ca/TheGoodSucc/iAntiCheat.lua"))()
 
-Library:Notification("CA bypass Injected", "Crystal Bypass", 5, Color3.fromRGB(255, 255 ,255))
+Library:Notification("CA bypass Injected", "Crystal Bypassed", 5, Color3.fromRGB(255, 255 ,255))
