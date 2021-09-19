253130ccbb3af3b57bf6fe5b073b5c36fa3035ad
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-09-19 19:24:56 -0400

    Delete Anti-Kick.lua

--- Anti-Kick.lua
+++
@@ -1,8 +0,0 @@
-local Old
-Old = hookmetamethod(game, "__namecall", function(Self, ...) 
-    if getnamecallmethod() == "Kick" then 
-        return wait(9e8)
-    end
-
-    return Old(Self, ...)
-end)
