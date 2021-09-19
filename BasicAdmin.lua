2aa55fb6965b13eb0cbca80f15ea2fd97068cdbd
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-09-19 19:22:33 -0400

    Delete BasicAdmin.lua

--- BasicAdmin.lua
+++
@@ -1,4 +0,0 @@
--- // Basic Exploit Essentials \\ --
--- // By reggie @v3rmillion.net | Nebylax#00001 \\ --
-
-loadstring(game:HttpGet('https://raw.githubusercontent.com/Nextrixcs/Basic-Exploit-Essentials/main/BasicExploitEssentials.lua'))()
