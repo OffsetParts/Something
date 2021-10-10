83ee6ad6204a763be57cb5a386d161e5ed905982
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-10-10 15:01:47 -0400

    Update NoNameTag.lua

--- NoNameTag.lua
+++ NoNameTag.lua
@@ -5,26 +5,29 @@ local HRP = charc:WaitForChild("HumanoidRootPart")
 
 Hum.DisplayDistanceType = Enum.HumanoidDisplayDistanceType.None
 
-local plr = game:GetService("Players").LocalPlayer
-local name = plr.Name
+--local player = game:GetService("Players").LocalPlayer
+local name = player.Name
 
-plr.CharacterAdded:Connect(function(charca)
-	wait(3)
-	for index, v in pairs(charca:GetDescendants()) do
-		if v:IsA("BillboardGui") then
-			if v.Name == name then
-				v:Destroy()
-			elseif v.Parent.Parent.Parent:GetDescendants() == name then
-				v:Destroy()
-			else
-				v:Destroy()
+player.CharacterAdded:Connect(function(charca)
+	if bl == false then
+		wait(3)
+		for index, v in pairs(charca:GetDescendants()) do
+			if v:IsA("BillboardGui") then
+				if v.Name == name then
+					v:Destroy()
+				elseif v.Parent.Parent.Parent:GetDescendants() == name then
+					v:Destroy()
+				else
+					v:Destroy()
+					if Logs then
+						print("NameTag Removed")
+					end
+				end
+					
 				if Logs then
 					print("NameTag Removed")
 				end
 			end
-				if Logs then
-					print("NameTag Removed")
-				end
 		end
 	end
 end)
