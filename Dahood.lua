4112b19eec9e08be80f6db33c707c2c7cf4f3346
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-10-19 15:55:43 -0400

    Create Dahood.lua

---
+++ Dahood.lua
@@ -0,0 +1,5 @@
+-- Stops people with premium dahood scripts to target you
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/Something/master/Da%20Hood%20Premium%20Stopper.lua"),true)()
+
+-- Old Anticheat Bypass
+loadstring(game:HttpGet(("https://github.com/Input50/Something/blob/master/DaHoodAntiCheatBypass.lua"),true)()
