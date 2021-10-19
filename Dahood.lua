9a0c1eb49fef40bea0c03ff846136ba7b9f9ccfe
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-10-19 15:56:17 -0400

    Update Dahood.lua

--- Dahood.lua
+++ Dahood.lua
@@ -2,4 +2,4 @@
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/Something/master/Da%20Hood%20Premium%20Stopper.lua"),true)()
 
 -- Old Anticheat Bypass
-loadstring(game:HttpGet(("https://github.com/Input50/Something/blob/master/DaHoodAntiCheatBypass.lua"),true)()
+loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/Something/master/DaHoodAntiCheatBypass.lua"),true)()
