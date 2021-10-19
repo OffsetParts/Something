5760655b80303a9bbadf4c843c15ae9fa4569ab2
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-10-19 15:49:15 -0400

    Add files via upload

---
+++ Roblox FriendlyFire Bypass.lua
@@ -0,0 +1,25 @@
+if not game:IsLoaded() then game.Loaded:Wait() end
+
+ pcall(function()
+               if game.Players.LocalPlayer.PlayerGui:FindFirstChild("Tracker") then
+
+         for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Tracker.Changed)) do
+        v:Disable();
+ end
+ for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Loop.Changed)) do
+        v:Disable();
+ end
+  for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Loop.AncestryChanged)) do
+        v:Disable();
+ end
+  for i,v in pairs(getconnections(game:GetService("Players").LocalPlayer.PlayerGui.Tracker.AncestryChanged)) do
+        v:Disable();
+  end
+
+game:GetService("Players").LocalPlayer.PlayerGui.Tracker.Disabled = true
+game:GetService("Players").LocalPlayer.PlayerGui.Loop.Disabled = true
+game:GetService("Players").LocalPlayer.PlayerGui.Tracker:Destroy()
+game:GetService("Players").LocalPlayer.PlayerGui.Loop:Destroy()
+
+end
+end)
