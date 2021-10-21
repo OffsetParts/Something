93068d91a3b9ba0a03d915f28237126ef5a52d48
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-10-21 17:13:22 -0400

    Update AntiCheat.lua

--- AntiCheat.lua
+++ AntiCheat.lua
@@ -1,37 +1,38 @@
 -- AntiCheatV2 by IrisV3rm
 -- if its anything else if will just execute default
+print("test")
 
 local bgames = {
     crystal = {
         Name = "Crystal",
         PlaceIDs = {6006653296, 5580097107, 5849392844},
-        ScriptToRun = "https://raw.githubusercontent.com/Input50/Something/master/CAC.lua",
+        ScriptToRun = "https://raw.githubusercontent.com/Input50/Something/master/CAC.lua"
     },
     
     adonis = {
         Name = "Adonis",
         PlaceIDs = {2772166173, 920587237, 286090429, 6539893534},
-        ScriptToRun = "https://raw.githubusercontent.com/Input50/Something/master/AAC.lua",
+        ScriptToRun = "https://raw.githubusercontent.com/Input50/Something/master/AAC.lua"
     },
     
     dahood = {
         Name = "Dahood",
-        PlaceIDs = {2788229376}
-        ScriptToRun = "https://raw.githubusercontent.com/Input50/Something/master/Da%20Hood%20Premium%20Stopper.lua",
+        PlaceIDs = {2788229376},
+        ScriptToRun = "https://raw.githubusercontent.com/Input50/Something/master/Da%20Hood%20Premium%20Stopper.lua"
     },
 
     lt2 = {
         Name = "LumberTycoon2",
         PlaceIDs = {13822889},
-        ScriptToRun = "https://raw.githubusercontent.com/Input50/Something/master/LT2-anticheatbypass.lua",
-    },
+        ScriptToRun = "https://raw.githubusercontent.com/Input50/Something/master/LT2-anticheatbypass.lua"
+    }
 }
 
 logs(".")
 un = true
-for _, bgame in pairs(bgames) do
-    for _, placeid in pairs(bgame.PlaceIDs) do
-        if placeid == game.PlaceId then
+for _, games in pairs(bgames) do
+    for _, placeid in ipairs(games.PlaceIDs) do
+        if placeid == game.placeId then
             loadstring(game:HttpGet((bgame.ScriptToRun),true))()
             un = false
         end
@@ -39,7 +40,7 @@ for _, bgame in pairs(bgames) do
 end
 logs("..")
 
-if un == true then
+if un then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Input50/Something/master/DAC.lua"))()
 end
 ("...")
