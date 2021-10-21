defc24762c8cc7a8ccd6b4999427e29690e7e245
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-10-21 15:34:04 -0400

    Add files via upload

---
+++ SlayersUnleasedAdmin.lua
@@ -0,0 +1,5 @@
+if not game:IsLoaded() then game.Loaded:Wait() end
+
+if game.placeId == 6536671967 then
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/XTheMasterX/Scripts/Main/SlayersUnleashedAdmin'),true))()
+end
