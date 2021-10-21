ccedf571bd1b4fa2291b4943a283407fba82e46e
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-10-21 15:34:32 -0400

    Update SlayersUnleasedAdmin.lua

--- SlayersUnleasedAdmin.lua
+++ SlayersUnleasedAdmin.lua
@@ -1,5 +1,3 @@
-if not game:IsLoaded() then game.Loaded:Wait() end
-
 if game.placeId == 6536671967 then
 loadstring(game:HttpGet(('https://raw.githubusercontent.com/XTheMasterX/Scripts/Main/SlayersUnleashedAdmin'),true))()
 end
