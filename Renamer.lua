08c999f98d9e978739a9fe08ae1e3e20d03abdaf
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-10-23 20:59:17 -0400

    Update Renamer.lua

--- Renamer.lua
+++ Renamer.lua
@@ -4,13 +4,13 @@ getgenv().name = _G.Name
 local plr = game.Players.LocalPlayer
 for Index, Value in next, game:GetDescendants() do 
     if Value.ClassName == "TextLabel" then 
-        local has = string.find(Value.Text,Plr.Name) 
+        local has = string.find(Value.Text,plr.Name) 
         if has then 
-            local str = Value.Text:gsub(Plr.Name,name)
+            local str = Value.Text:gsub(plr.Name,name)
             Value.Text = str 
         end
         Value:GetPropertyChangedSignal("Text"):Connect(function()
-            local str = Value.Text:gsub(Plr.Name,name)
+            local str = Value.Text:gsub(plr.Name,name)
             Value.Text = str 
         end)
     end
@@ -18,13 +18,13 @@ end
 
 game.DescendantAdded:Connect(function(Value)
     if Value.ClassName == "TextLabel" then 
-        local has = string.find(Value.Text,Plr.Name)
+        local has = string.find(Value.Text,plr.Name)
         Value:GetPropertyChangedSignal("Text"):Connect(function()
-            local str = Value.Text:gsub(Plr.Name,name)
+            local str = Value.Text:gsub(plr.Name,name)
             Value.Text = str 
         end)
         if has then 
-            local str = Value.Text:gsub(Plr.Name,name)
+            local str = Value.Text:gsub(plr.Name,name)
             Value.Text = str 
         end
         
