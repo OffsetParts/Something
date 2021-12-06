a5bf17ac8cc0ec83c9dd925befca929ab287b534
Nope Nata <44343694+Input50@users.noreply.github.com>
2021-12-05 20:41:27 -0500

    Update Noclipper.lua

--- Noclipper.lua
+++ Noclipper.lua
@@ -1,7 +1,23 @@
 local plr = game:GetService("Players").LocalPlayer
-loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/Something/master/Noclip.lua"),true))()
 
-plr.CharacterAdded:Connect(function()
-wait(0.2)
-    loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/Something/master/Noclip.lua"),true))()
-end)
+local blacklist = {
+    0000000,
+}
+
+function Noclip()
+    plr.CharacterAdded:Connect(function()
+        loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/Something/master/Noclip.lua"),true))()
+    end
+end
+    
+Noclip()
+
+for _, games in pairs(blacklist) do
+    if game.placeId == games then
+        if _G.Logs then
+            print('Game is blacklisted cant execute due to ac detection risk')
+        end
+    else
+        Noclip()
+    end
+end
