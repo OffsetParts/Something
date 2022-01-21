a81b6727660c7c3e93184cb569d3e2509067b267
Nope Nata <44343694+Input50@users.noreply.github.com>
2022-01-21 15:30:52 -0500

    Update Noclipper.lua

--- Noclipper.lua
+++ Noclipper.lua
@@ -2,19 +2,22 @@ if not game:IsLoaded() then game.Loaded:Wait() end
 
 local plr = game:GetService("Players").LocalPlayer
 
+local exist
 local function check()
     local backpack = plr:FindFirstChildOfClass("Backpack")
-    if backpack ~= nil then return else return true end
+    if backpack ~= nil then exist = true else return end
 end
+
+check()
 
 function Noclip()
     plr.CharacterAdded:Connect(function()
-		if check() == true then
+        check()
+		if exist == true then
 			loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/Something/master/Noclip.lua"),true))()
+		    print('noclip')
 		end
     end)
 end
 
-if check() then
-	Noclip()
-end
+Noclip()
