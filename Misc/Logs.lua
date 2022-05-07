475a4a33f3a65b8230cd46bcd5fcf35046750543
Nope Nata <lifeisalie309@gmail.com>
2022-05-06 21:08:23 -0400

    Update Logs.lua

--- Misc/Logs.lua
+++ Misc/Logs.lua
@@ -5,20 +5,20 @@ function localTime(date)
     local result = string.gsub(dataString, "{(%w+)}", date)
     return result
 end
-local now = os.time()
-
-local index = 1
 
 
 -- changes locallogs for anyone who trys to get it it won't change for you GetLogHistory
 -- Full protection with sensible logs that are common to confuse any dev or admin
+
 local Old
+local now = os.time()
+
 Old = hookfunction(LogService.GetLogHistory, function(...)
     if checkcaller() then return Old(...) end
 return {
     {
 		-- 16 chars
-        message = "[" .. localTime(os.date("*t", now)) .. "] " .. "Failed to load sound rbxassetid://296178266: Unable to download sound data",
+        message = "[" .. localTime(os.date("*t", now)) .. "] " .. "Failed to load sound rbxassetid://5486216: Unable to download sound data",
         messageType = Enum.MessageType.MessageError,
         timeStamp = 0
     },
