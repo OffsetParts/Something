d4832c95784c206cd10c3400b4e4286715fb6e09
Nope Nata <44343694+Input50@users.noreply.github.com>
2022-07-26 17:38:25 -0400

    I got a glock in my rari

--- Template.lua
+++ Template.lua
@@ -1,31 +1,32 @@
--- ver - 3.2 | Its getting there I think
-setfflag("AbuseReportScreenshotPercentage", 0)
-setfflag("DFFlagAbuseReportScreenshot", "False")
-setfflag("DFStringCrashPadUploadToBacktraceToBacktraceBaseUrl", "")
-setfflag("DFIntCrashUploadToBacktracePercentage", "0")
-setfflag("DFStringCrashUploadToBacktraceBlackholeToken", "")
-setfflag("DFStringCrashUploadToBacktraceWindowsPlayerToken", "")
--- ^my dumbass forgot they supposed to load before the game loads completely
+-- ver - 3.5 | Its getting there I think
+setfflag("AbuseReportScreenshotPercentage", 0) -- nullifies it chances
+setfflag("DFFlagAbuseReportScreenshot", "False") -- Disable Abuse Report Screenshot
+setfflag("DFStringCrashPadUploadToBacktraceToBacktraceBaseUrl", "") -- remove the url it will upload crash report to prevent logging
+setfflag("DFIntCrashUploadToBacktracePercentage", "0") -- nullifies the chances of it happening
+setfflag("DFStringCrashUploadToBacktraceBlackholeToken", "") -- remove fingerprinting token
+setfflag("DFStringCrashUploadToBacktraceWindowsPlayerToken", "") -- remove fingerprinting token
 
+-- loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/Something/master/Main/Bypass.lua"), true))()
 if not game:IsLoaded() then game.Loaded:Wait() end
+_senv = getgenv() or _G
 
 local ST = os.clock()
 -- [ Settings ] -- At the top for quicker access
 getgenv().config = {
-	ACBs = false,   -- Community gathered Anticheat bypasses | Only contributor me :(
+	ACBs = true,   -- Community gathered Anticheat bypasses | Only contributor me :(
     NR   = false,   -- Name replacer | Replaces your name in-game every clientsided
-	NTR  = false,   -- NameTag Remover | An function to find any client side nametags to remove (caution: raises suspicion)
-	NC   = false,  -- Noclip tool
+	NTR  = true,  -- NameTag Remover | An function to find any client side nametags to remove (caution: raises suspicion)
+	NC   = true,   -- Noclip tool
 	ASS  = false,  -- Anti-Stream Snipe | Function Denaming players to make it harder to track your games. Tip: Interferes with ADN so choose wisely
     ADN  = {       -- Anti Display Names by mothra#4150
-		Enable = false,
+		Enable = true,
 		Preferences = {
 			RetroNaming = false,
 			ShowOriginalName   = true,
 			ApplyToLeaderboard = true,
 			IdentifyFriends    = {Toggle = true, Identifier = '[Cuz]'},
 			IdentifyBlocked    = {Toggle = true, Identifier = '[Cunt]'},
-			IdentifyPremium    = {Toggle = false, Identifier = '[Premium]'},
+			IdentifyPremium    = {Toggle = true, Identifier = '[Premium]'},
 			IdentifyDeveloper  = {Toggle = true, Identifier = '[Developer]'},
 			SpoofLocalPlayer   = {Toggle = false, UseRandomName = false, NewName = 'Random Name Lol'},
 			Orientation 	   = 'Vertical'
@@ -41,33 +42,21 @@ getgenv().config = {
 }
 
 -- [[ Variables ]] --
-if not Scrumpy then getgenv()["Scrumpy"] = {} end -- Yes I named it that, its to make it harder for other scripts global to interfere
-local Settings = getgenv()["Scrumpy"]
-
-getgenv().Alias = 'Nil'
-Settings = {
+getgenv()["Scrumpy"] = {-- Yes I named it that, so its to make it harder for other scripts global to interfere
+	Alias = 'Nil',
 	Logs  = true, -- Enable logs
-	Debug = true, -- Unlocks more intensive features for debugging | WIP
+	Debug = true
 }
 
 getgenv().Notifier = function(str, debug) -- global quick print function
-    if Settings.Logs then
+    if  getgenv()["Scrumpy"].Logs then
         if not debug then
             print(tostring(str))
-        elseif Settings.Debug then
+        elseif  getgenv()["Scrumpy"].Debug then
             warn("[DEBUG] " .. tostring(str))
         end
     end
 end
-
--- [[ Libraries ]] -- | <3 iris zaddy
-
-if not IrisInit or not Iris then
-    loadstring(game:HttpGet("https://irishost.xyz/InfinityHosting/IrisInit.lua"))()
-	loadstring(game:HttpGet("https://api.irisapp.ca/Scripts/IrisBetterConsole.lua"))() 
-	loadstring(game:HttpGet("https://api.irisapp.ca/Scripts/IrisBetterCompat.lua"))()
-end
-
 
 loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/Something/master/Utilites/Settings.lua"), true))()
 Notifier("(1) Settings Loaded", true)
@@ -79,12 +68,11 @@ task.spawn(function ()
 	end
 	task.delay(7, function () 
         Notifier("Loading...", true)
-		--- Obscure Names
 		if config.NR then
 			loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/Something/master/Main/Renamer.lua"), true))()
 			Notifier("(3) Obscurer Planted", true)
 		end
-		
+		-----------------------------------------------------------------------------------------------------------------------
 		if config.NTR then -- lol
 			loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/Something/master/Main/NameTag.lua"), true))()
 			Notifier("(4) Nametag Remover Working", true)
@@ -100,15 +88,14 @@ task.spawn(function ()
 			Notifier("(6) Anti-Stream Snipe established", true)
 		end
 		-----------------------------------------------------------------------------------------------------------------------
-		-- Noclip
 		if config.NC then
 			loadstring(game:HttpGet(("https://raw.githubusercontent.com/Input50/Something/master/Main/Noclip.lua"), true))()
 			Notifier("(7) Noclip Launched", true)
 		end
+		
+		DT = os.clock() - ST
+		Notifier("Took " .. DT .. " seconds to load", true)
 	end)
-
-	DT = os.clock() - ST
-	Notifier("Took " .. DT .. " seconds to load", true)
 end)
 
 --- Custom | Possibly more addons in the future
