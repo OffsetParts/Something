ba1139f1cadbe1d72b23b85462f59a279ad1e580
Nope Nata <44343694+Input50@users.noreply.github.com>
2022-08-03 11:07:34 -0400

    EZ

--- AC Bypass/Games/Combat Warriors.lua
+++ AC Bypass/Games/Combat Warriors.lua
@@ -1,100 +1,54 @@
-local fw = require(game.ReplicatedStorage.Framework.Nevermore)
-syn_context_set(2)
-local acc = fw("AntiCheatHandlerClient")
-syn_context_set(7)
-local func = acc.createNotification
-hookfunc(
-    func,
-    function()
-        return
-    end
-)
-local hook
-hook =
-    hookmetamethod(
-    game,
-    "__namecall",
-    function(self, ...)
-        if getnamecallmethod() == "FireServer" and self.Name == "LogKick" then
-            return wait(9e9)
-        end
-        if getcallingscript() == "AntiCheatHandler" and getnamecallmethod() == "Destroy" then
-            return wait(9e9)
-        end
-        if getnamecallmethod() == "Kick" and not checkcaller() then
-            return wait(9e9)
-        end
+local Nevermore = require(game.ReplicatedStorage.Framework.Nevermore)
+local Lookup = rawget(Nevermore, '_lookupTable')
+local function GetModule(Name)
+   local Module = rawget(Lookup, Name)
 
-        return hook(self, ...)
-    end
-)
-nhk =
-    hookfunc(
-    Instance.new,
-    function(...)
-        if getcallingscript() == "AntiCheatHandler" then
-            return wait(9e9)
-        end
-        return nhk(...)
-    end
-)
-lolz =
-    hookfunc(
-    task.spawn,
-    function(...)
-        if getcallingscript() == "AntiCheatHandler" then
-            return wait(9e9)
-        end
-        if getcallingscript() == "AntiCheatHandlerClient" then
-            return wait(9e9)
-        end
-        return lolz(...)
-    end
-)
-syn_context_set(2)
-local acc = fw("Flag")
-syn_context_set(7)
+   if Module then
+       return require(Module)
+   end
+end
 
-local func = acc.setFlags
-local func1 = acc.addFlags
-local func2 = acc.getIsMaxed
-local func3 = acc.getFlags
-hookfunc(
-    func,
-    function()
-        return wait(9e9)
-    end
-)
-hookfunc(
-    func1,
-    function()
-        return wait(9e9)
-    end
-)
-hookfunc(
-    func2,
-    function()
-        return wait(9e9)
-    end
-)
-hookfunc(
-    func3,
-    function()
-        return wait(9e9)
-    end
-)
-syn_context_set(2)
-hookfunc(
-    string.format,
-    function()
-        if getcallingscript() == "AntiCheatHandler" then
-            return
-        end
-    end
-)
-hookfunc(
-    fw("AntiCheatHandler").punish,
-    function()
-        return
-    end
-)
+
+local Anticheat = GetModule('AntiCheatHandler')
+local ClientAnticheat = GetModule('AntiCheatHandlerClient')
+local NotificationHandler = GetModule('CoreGuiHandlerClient')
+
+local SendNotification
+if NotificationHandler then
+   SendNotification = rawget(NotificationHandler, 'sendNotification')
+end
+
+
+if Anticheat then
+   local Punish = rawget(Anticheat, 'punish')
+   if Punish then
+       if SendNotification then
+           SendNotification({
+               Title = '[BYPASS] Client anticheat hooked',
+               Text = 'All non-server punishments will be subverted'
+           })
+       end
+
+       hookfunction(Punish, function(Player, Info, Callback)
+           print('Punishment attempted')
+
+           if Info and type(Info) == 'table' then
+               table.foreach(Info, warn)
+           else
+               warn('No extra info')
+           end
+       end)
+   end
+end
+
+if ClientAnticheat then
+   local CreateNotification = rawget(ClientAnticheat, 'createNotification')
+   if CreateNotification then
+       hookfunction(CreateNotification, function(MessageInfo)
+           return SendNotification({
+               Title = '[BYPASS] Server anticheat caught you',
+               Text = 'Hooking cannot stop the server'
+           })
+       end)
+   end
+end
