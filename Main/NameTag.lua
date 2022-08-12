6ba3b0aba56d66d921043b7eea60f3ea995d82ab
Nope Nata <44343694+Input50@users.noreply.github.com>
2022-08-12 09:34:01 -0400

    Fix

--- Main/NameTag.lua
+++ Main/NameTag.lua
@@ -1,15 +1,19 @@
 -- So there is this guy that made a Anti-fling and for this update i skidded his method of monitoring the LP Character and service which i must say is cool as fuck. So i just stole it and it works really well.
 
 -- [ Services ] --
-local Services = setmetatable({}, {
-	__index = function(Self, Index)
-		local NewService = game.GetService(game, Index)
-		if NewService then
-			Self[Index] = NewService
-		end
-		return NewService
-	end
-})
+local Services =
+    setmetatable(
+    {},
+    {
+        __index = function(Self, Index)
+            local NewService = game.GetService(game, Index)
+            if NewService then
+                Self[Index] = NewService
+            end
+            return NewService
+        end
+    }
+)
 
 local Workspace = Services.Workspace
 local plr = Services.Players.LocalPlayer
@@ -69,4 +73,8 @@ local function CharacterAdded(NewCharacter)
 end
 
 CharacterAdded(plr.Character or plr.CharacterAdded:Wait())
-plr.CharacterAdded:Connect(CharacterAdded())
+plr.CharacterAdded:Connect(
+    function(chara)
+        CharacterAdded(chara)
+    end
+)
