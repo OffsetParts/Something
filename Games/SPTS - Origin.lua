34904dcb5b70581ee4bce55ed62c1ebe5cd3f7d1
Nope Nata <44343694+Input50@users.noreply.github.com>
2022-09-25 20:06:07 -0400

    Ez

---
+++ Games/SPTS - Origin.lua
@@ -0,0 +1,337 @@
+-- < Services > --
+local CoreGui               = game:GetService("CoreGui")
+local Players               = game:GetService("Players")
+local Workspace             = game:GetService("Workspace")
+local RunService            = game:GetService("RunService")
+local ReplicatedStorage     = game:GetService("ReplicatedStorage")
+
+-- < Variables > --
+local localPlayer           = Players.LocalPlayer
+
+local Character = localPlayer.Character or localPlayer.CharacterAdded:Wait()
+local HRP = Character:WaitForChild("HumanoidRootPart")
+
+local TAs = Workspace:WaitForChild("Map"):WaitForChild("Training_Collisions")
+
+local Settings = {
+    FMS = false,
+    FFS = false,
+    FBT = false,
+    FJF = false,
+    FPP = false
+}
+
+local Multipliers = {
+    FS = 1,
+    MS = 1,
+    BT = 1,
+    JF = 1,
+    PP = 1
+}
+
+-- < Functions > --
+function getList(Parent: Instance)
+    local list = {}
+    for _,v in pairs(Parent:GetChildren()) do
+        table.insert(list, v.Name)
+    end
+    return list
+end
+
+-- < GUI > --
+local OrionLib     = loadstring(game:HttpGetAsync(('https://raw.githubusercontent.com/shlexware/Orion/main/source'), true))()
+local Orion        = CoreGui:FindFirstChild("Orion")
+
+local MainWindow   = OrionLib:MakeWindow({Name = "SPTS - Origin", HidePremium = false, SaveConfig = true, ConfigFolder = "./Scrumpy/SPTS", IntroEnabled = true, IntroText    = "Amigos",})
+
+local MLT    = MainWindow:MakeTab({Name = ' Multipliers '})
+local AF     = MainWindow:MakeTab({Name = ' Auto Farm '})
+local TP     = MainWindow:MakeTab({Name = ' Teleports '})
+local Misc   = MainWindow:MakeTab({Name = ' Misc '})
+
+local Info   = MLT:AddSection({Name = ' Info '})
+local Pliers = MLT:AddSection({Name = ' Pliers '})
+
+Info:AddParagraph("Quick Note", "You can't just go max multi on all. You will die and you won't even get reward unless its you qualify for it. Find out what multipliers you can use and farm them till you reach the next one, this goes for weights aswell. If you want to know when you qualify for the next one, search it on the wiki fuckhead.")
+Info:AddParagraph("Example", "Fist Strength - Rock(100 FS), Crystal(10K), same for the others. For everything Default is always 1")
+Pliers:AddParagraph("Bad news","BT gained from training areas are serversided, so you can't change it. Just farm everything else in your BT zone")
+AF:AddLabel("Can't not train FS and BT at the same time.")
+
+Pliers:AddDropdown({
+    Name = "Fist Strength",
+    Default = "Default",
+    Options = {
+        "Default",
+        "Rock",
+        "Crystal",
+        "Blue God Star",
+        "Green God Star",
+        "Red God Star",
+    },
+    Callback = function(value)
+        local index = {
+            ["Default"] = 1,
+            ["Rock"] = 2,
+            ["Crystal"] = 3,
+            ["Blue God Star"] = 4,
+            ["Green God Star"] = 5,
+            ["Red God Star"] = 6,
+        }
+        Multipliers.FS = index[value]
+    end,
+    Flag = "FSM",
+    Save = true
+})
+
+--[[ Pliers:AddDropdown({
+    Name = "Body Toughness",
+    Default = "Default",
+    Options = {
+        "Default",
+        "Acid_b",
+        "Fire_b",
+        "Iceberg",
+        "Tornado",
+        "Valcano",
+    },
+    Callback = function(value)
+        local index = {
+            ["Default"] = 1,
+            ["Acid_b"] = 2,
+            ["Fire_b"] = 3,
+            ["Iceberg"] = 4,
+            ["Tornado"] = 5,
+            ["Valcano"] = 6
+        }
+        Multipliers.BT = index[value]
+    end,
+    Flag = "BTM",
+    Save = true
+}) ]]
+
+Pliers:AddDropdown({
+    Name = "Psychic Power",
+    Default = "Default",
+    Options = {
+        "Default",
+        "Flying",
+        "First_Lawn",
+        "Second_Lawn",
+        "Bridge",
+        "Waterfall"
+    },
+    Callback = function(value)
+        local index = {
+            ["Default"] = 1,
+            ["Flying"] = 2,
+            ["First_Lawn"] = 3,
+            ["Second_Lawn"] = 4,
+            ["Bridge"] = 5,
+            ["Waterfall"] = 6
+        }
+        Multipliers.PP = index[value]
+    end,
+    Flag = "PPM",
+    Save = true
+})
+
+Pliers:AddDropdown({
+    Name = "Movement Speed",
+    Default = "Default",
+    Options = {
+        "Default",
+        "100_LB",
+        "1_TON",
+        "10_TON",
+        "100_TON",
+    },
+    Callback = function(value)
+        local index = {
+            ["Default"] = 1,
+            ["100_LB"] = 2,
+            ["1_TON"] = 3,
+            ["10_TON"] = 4,
+            ["100_TON"] = 5,
+        }
+        Multipliers.MS = index[value]
+    end,
+    Flag = "MSM",
+    Save = true
+})
+
+Pliers:AddDropdown({
+    Name = "Jump Force",
+    Default = "Default",
+    Options = {
+        "Default",
+        "100_LB",
+        "1_TON",
+        "10_TON",
+        "100_TON",
+    },
+    Callback = function(value)
+        local index = {
+            ["Default"] = 1,
+            ["100_LB"] = 2,
+            ["1_TON"] = 3,
+            ["10_TON"] = 4,
+            ["100_TON"] = 5,
+        }
+        Multipliers.JF = index[value]
+    end,
+    Flag = "JFM",
+    Save = true
+})
+
+AF:AddToggle({
+    Name = "Auto MS",
+    Note = "Farm Movement Speed",
+    Default = false,
+    Callback = function(Value)
+        Settings.FMS = Value
+        if Settings.FMS then
+            task.spawn(function()
+                while Settings.FMS do
+                    local args = {
+                        [1] = "Add_MS_Request",
+                        [2] = Multipliers.MS
+                    }
+                    ReplicatedStorage.RemoteEvent:FireServer(args)
+                    task.wait(1) -- there is a cooldown to when you can fire these, cry about it
+                end
+            end)
+        end
+    end,
+    Flag = "MS",
+    Save = true
+})
+
+AF:AddToggle({
+    Name = "Auto JF",
+    Note = "Farm Jump Force",
+    Default = false,
+    Callback = function(Value)
+        Settings.FJF = Value
+        if Settings.FJF then
+            task.spawn(function()
+                while Settings.FJF do
+                    local args = {
+                        [1] = "Add_JF_Request",
+                        [2] = Multipliers.JF
+                    }
+                    ReplicatedStorage.RemoteEvent:FireServer(args)
+                    task.wait(1) -- there is a cooldown to when you can fire these, cry about it
+                end
+            end)
+        end
+    end,
+    Flag = "JF",
+    Save = true
+})
+
+AF:AddToggle({
+    Name = "Auto FS",
+    Note = "Farm Fist Strength",
+    Default = false,
+    Callback = function(Value)
+        Settings.FFS = Value
+        if Settings.FFS then
+            task.spawn(function()
+                while Settings.FFS do
+                    local args = {
+                        [1] = "+FS" .. tostring(Multipliers.FS),
+                    }
+                    ReplicatedStorage.RemoteEvent:FireServer(args)
+                    task.wait(1)
+                end
+            end)
+        end
+    end,
+    Flag = "FS",
+    Save = true
+})
+
+--[[ AF:AddToggle({
+    Name = "Auto BT",
+    Note = "Farm Body Toughness",
+    Default = false,
+    Callback = function(Value)
+        Settings.FBT = Value
+        if Settings.FBT then
+            task.spawn(function()
+                while Settings.FBT do
+                    local args = {
+                        [1] = "+BT" .. tostring(Multipliers.BT),
+                    }
+                    ReplicatedStorage.RemoteEvent:FireServer(args)
+                    task.wait(1.25)
+                end
+            end)
+        end
+    end,
+    Flag = "BT",
+    Save = true
+}) ]]
+
+AF:AddToggle({
+    Name = "Auto PP",
+    Note = "Farm Psychic Power",
+    Default = false,
+    Callback = function(Value)
+        Settings.FPP = Value
+        if Settings.FPP then
+            task.spawn(function()
+                while Settings.FPP do
+                    local args = {
+                        [1] = "+PP" .. tostring(Multipliers.PP),
+                    }
+                    ReplicatedStorage.RemoteEvent:FireServer(args)
+                    task.wait(1.5)
+                end
+            end)
+        end
+    end,
+    Flag = "PP",
+    Save = true
+})
+
+TP:AddDropdown({
+    Name = "TP to FS training area",
+    Default = "Rock",
+    Options = getList(TAs:WaitForChild("FistStrength")),
+    Callback = function(value)
+        local tpTo = TAs:WaitForChild("FistStrength"):WaitForChild(value)
+        HRP.CFrame = tpTo.CFrame
+    end
+})
+
+TP:AddDropdown({
+    Name = "TP to BT training area",
+    Default = "Rock",
+    Options = getList(TAs:WaitForChild("BodyToughness")),
+    Callback = function(value)
+        local tpTo = TAs:WaitForChild("BodyToughness"):WaitForChild(value)
+        HRP.CFrame = tpTo.CFrame
+    end
+})
+
+TP:AddDropdown({
+    Name = "TP to PP training area",
+    Default = "Rock",
+    Options = getList(TAs:WaitForChild("PsychicPower")),
+    Callback = function(value)
+        local tpTo = TAs:WaitForChild("PsychicPower"):WaitForChild(value)
+        HRP.CFrame = tpTo.CFrame
+    end
+})
+
+Misc:AddBind({
+    Name = "Control Gui",
+    Default = Enum.KeyCode.RightShift,
+    Hold = false,
+    Callback = function() 
+        Orion.Enabled = not Orion.Enabled 
+    end,
+    Flag = "GUI",
+    Save = true,
+})
